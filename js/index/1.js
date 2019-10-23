943ea6c428a9157875dc921f8c45fbd2ed9954d9
Barry <13396298960@163.com>
2019-10-23 21:13:06 +0800

    博客上传

---
+++ js/index/1.js
@@ -0,0 +1,3 @@
+// index-1-userCard
+$('.index-1-userCard-connection').height(300 - $('.index-1-userCard-about').height() - 5);
+$('.index-1-userCard-connection>div').css('line-height', $('.index-1-userCard-connection').height() + 'px');
